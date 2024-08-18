ae2466850e5d755fa75d9ed082c166b4ac7be98c
Richard Horton <richardhorton1312@gmail.com>
2024-08-18 21:58:06 +0100

    Created GridSetup function

--- Assets/Scripts/AutomataManager.cs
+++ Assets/Scripts/AutomataManager.cs
@@ -2,15 +2,82 @@ using UnityEngine;
 
 public class AutomataManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Cell prefabCell;
+
+    public int horizontalSize = 50;
+    public int verticalSize = 50;
+
+    public Cell[,] automataGrid;
+
+    void Awake()
     {
-        
+        setupGrid();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void setupGrid()
+    {
+        automataGrid = new Cell[horizontalSize, verticalSize];
+
+        for(int x = 0; x < horizontalSize; x++)
+        {
+            for(int y = 0; y < verticalSize; y++)
+            {
+                automataGrid[x, y] = Instantiate<Cell>(prefabCell);
+            }
+        }
+
+        for(int x = 0; x < horizontalSize; x++)
+        {
+            for(int y = 0; y < verticalSize; y++)
+            {
+                Cell currentCell = automataGrid[x, y];
+                currentCell.name = "Cell " + x + ", " + y;
+                currentCell.SetCoords(x, y);
+                currentCell.transform.position = new Vector3(x, y, 0);
+
+                Cell top, bottom, left, right;
+                
+                if(x == 0)
+                {
+                    left = null;
+                    right = automataGrid[x+1, y];
+                }
+                else if(x == horizontalSize-1)
+                {
+                    left = automataGrid[x-1, y];
+                    right = null;
+                }
+                else
+                {
+                    left = automataGrid[x-1, y];
+                    right = automataGrid[x+1, y];
+                }
+
+                if(y == 0)
+                {
+                    top = automataGrid[x, y+1];
+                    bottom = null;
+                }
+                else if(y == verticalSize-1)
+                {
+                    top = null;
+                    bottom = automataGrid[x, y-1];
+                }
+                else
+                {
+                    top = automataGrid[x, y+1];
+                    bottom = automataGrid[x, y-1];
+                }
+
+                currentCell.VonNeumannNeighbours(top, bottom, left, right);
+            }
+        }
         
     }
 }
