0d287a528db943b313f423d72423f7d9b25c55d1
Richard Horton <richardhorton1312@gmail.com>
2024-08-18 23:44:56 +0100

    Added State to cells

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
+    // Coordinates of this cell
     int cellX, cellY;
+
+    // 1 = Alive, 0 = Dead
+    public int state = -1;
+
+    public bool changedState = true;
 
     // von Neumann Neighbours
     Cell topNeighbour, bottomNeighbour, leftNeighbour, rightNeighbour;
@@ -31,5 +37,26 @@ public class Cell : MonoBehaviour
         bottomNeighbour = bottom;
         leftNeighbour = left;
         rightNeighbour = right;
+    }
+
+    // Mark if the state changed in the last cycle
+    public void UpdateState(int newState)
+    {
+        if(newState == state)
+        {
+            changedState = false;
+        }
+        else
+        {
+            // If the state changed, store the new state
+            state = newState;
+            changedState = true;
+        }
+    }
+
+    // Once the material has been updated, this information can be reset
+    public void UpdatedMaterial()
+    {
+        changedState = false;
     }
 }
